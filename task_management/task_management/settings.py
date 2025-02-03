847a8d0e3b2cc9a1a3522b5819e6ea2a2c403397
Dimash Amalbek <dimashamalbek@MacBook-Air-Dimash-2.local>
2025-02-04 01:12:56 +0500

    project

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -94,6 +94,12 @@ REST_FRAMEWORK = {
 
     ], 
 
+     'DEFAULT_AUTHENTICATION_CLASSES': [ 
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+
+    ], 
+
     'PAGE_SIZE': 10, 
 
 } 
@@ -165,3 +171,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
+
